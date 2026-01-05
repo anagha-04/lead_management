cfc21f5533ff1e77a14c732c65cc2986ce8c668d
anagha-04 <anaghanilkumar04@gmail.com>
2026-01-05 19:02:07 +0530

    list done

--- user_app/views.py
+++ user_app/views.py
@@ -57,6 +57,12 @@ class StudentAddview(APIView):
             return Response(serializer.data,status=status.HTTP_201_CREATED)
         
         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    
+    def get(self,request):
 
+        data = StudentLeadModel.objects.filter(user = request.user)
 
+        serializer = StudentLeadSerializer(data,many=True)
+
+        return Response(serializer.data,status=status.HTTP_200_OK)
     
