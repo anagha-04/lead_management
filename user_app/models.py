8515418a6c26cefd6f51689c9be510754835636f
anagha-04 <anaghanilkumar04@gmail.com>
2026-01-05 10:49:43 +0530

    models

--- user_app/models.py
+++ user_app/models.py
@@ -4,22 +4,22 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-# class StudentLeadModel(models.Model):
+class StudentLeadModel(models.Model):
 
-#       Source = [
-#         ("FRIEND","friend"),
-#         ("INSTAGRAM","instagram"),
-#         ("OWN","own"),
+      Source = [
+        ("FRIEND","friend"),
+        ("INSTAGRAM","instagram"),
+        ("OWN","own"),
     
-#       ]
-#       student_name= models.CharField(max_length=50)
+      ]
+      student_name= models.CharField(max_length=50)
 
-#       Qualification = models.CharField(max_length=50)
+      Qualification = models.CharField(max_length=50)
 
-#       phone_number = models.IntegerField(max_length=12)
+      phone_number = models.IntegerField(max_length=12)
 
-#       user = models.ForeignKey(User,on_delete=models.CASCADE)
+      user = models.ForeignKey(User,on_delete=models.CASCADE)
 
-#       Source =models.CharField(max_length=30,choices=Source)
+      Source =models.CharField(max_length=30,choices=Source)
 
-#       created_date = models.DateField(auto_now_add=True)
+      created_date = models.DateField(auto_now_add=True)
